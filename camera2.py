ac5d413b4708eef7bbb338d8584cb53f00d454b3
Francisco <fran.abm94@gmail.com>
2020-05-25 14:31:22 +0200

    changed the image to bytes

--- camera2.py
+++ camera2.py
@@ -1,11 +1,11 @@
 from imutils.video import VideoStream
 import cv2
-
+import numpy as np
 
 def imageGenerator():
     cap = cv2.VideoCapture(1)
     ret, frame = cap.read()
-    yield frame
+    yield (frame.tobytes())
     # yield (b'--frame\r\n'
     #     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
