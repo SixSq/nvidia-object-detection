6894f0f6c49f7ff7738e595ff614cc69cced1af9
Francisco <fran.abm94@gmail.com>
2020-05-25 14:35:34 +0200

    fixing error in encoding

--- camera2.py
+++ camera2.py
@@ -6,7 +6,7 @@ def imageGenerator():
     cap = cv2.VideoCapture(1)
     ret, frame = cap.read()
     ret, jpeg = cv2.imencode('.jpg', frame,
-                                 (cv2.IMWRITE_JPEG_QUALITY, self.quality))
+                                 (cv2.IMWRITE_JPEG_QUALITY, 80))
     yield (b'--frame\r\n'
         b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n')
 
