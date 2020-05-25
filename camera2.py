2120da40a26ce98fe5d38aef2a165d831fe4dcf7
Francisco <fran.abm94@gmail.com>
2020-05-25 14:29:00 +0200

    fixing yielding of frame

--- camera2.py
+++ camera2.py
@@ -5,8 +5,9 @@ import cv2
 def imageGenerator():
     cap = cv2.VideoCapture(1)
     ret, frame = cap.read()
-    yield (b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    yield frame
+    # yield (b'--frame\r\n'
+    #     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 if __name__ == "__main__":
