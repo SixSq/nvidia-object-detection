0840d3856b2b3417d4654310eaa7b9565acf8750
Francisco <fran.abm94@gmail.com>
2020-05-25 14:34:32 +0200

    encoded the image to jpeg

--- camera2.py
+++ camera2.py
@@ -5,8 +5,10 @@ import numpy as np
 def imageGenerator():
     cap = cv2.VideoCapture(1)
     ret, frame = cap.read()
+    ret, jpeg = cv2.imencode('.jpg', frame,
+                                 (cv2.IMWRITE_JPEG_QUALITY, self.quality))
     yield (b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
+        b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n')
 
 
 if __name__ == "__main__":
